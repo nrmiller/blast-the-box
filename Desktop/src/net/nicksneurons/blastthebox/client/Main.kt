9706e53a1aac2729f4fdda89f11ff40e2014be8d
Nicholas Miller <nrmiller923@outlook.com>
2023-05-29 08:21:49 -0400

    Configure project for MacOS

--- Desktop/src/net/nicksneurons/blastthebox/client/Main.kt
+++ Desktop/src/net/nicksneurons/blastthebox/client/Main.kt
@@ -2,10 +2,17 @@ package net.nicksneurons.blastthebox.client
 
 import com.fractaldungeon.tools.GLWindow
 import com.fractaldungeon.tools.GLWindowListener
+import com.fractaldungeon.tools.GameWindow
 
 import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFW.glfwInit
 
 fun main() {
+
+    glfwInit()
+    val window = GameWindow("My Game", 600, 480)
+
+    window.showDialog()
 }
 
 class Main : GLWindowListener {
